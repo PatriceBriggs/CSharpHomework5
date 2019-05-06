5e0f88b84321b084886d170c6802b7103513e3b7
Patrice Briggs <PBriggs@perkinscoie.com>
2019-05-06 16:23:20 -0700

    Initial Commit Homework5

---
+++ Homework5/MainWindow.xaml.cs
@@ -0,0 +1,173 @@
+﻿using Homework5.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Homework5
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        TicTacToeGame ttt = new TicTacToeGame();
+        public MainWindow()
+        {
+
+            InitializeComponent();
+            uxTurn.Text = "Player X Turn";
+            ttt.GameStatus = "Player X Turn";
+            ttt.PlayerXTurn = true;
+            ttt.PlayerOTurn = false;
+            ttt.MoveNumber = 1;           
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string thisTag = ((Button)sender).Tag.ToString();
+
+            SetSquareValue(thisTag, ttt.PlayerXTurn);          
+
+            if (ttt.PlayerXTurn)
+            { 
+                (sender as Button).Content = "X";
+                ttt.PlayerXTurn = false;
+                ttt.PlayerOTurn = true;
+                ttt.GameStatus = ttt.IsAWinner(ttt.GameStatus,ttt.MoveNumber);
+                uxTurn.Text = ttt.GameStatus;
+            }
+            else { 
+                (sender as Button).Content = "O";
+                ttt.PlayerXTurn = true;
+                ttt.PlayerOTurn = false;
+                ttt.GameStatus = ttt.IsAWinner(ttt.GameStatus, ttt.MoveNumber);
+                uxTurn.Text = ttt.GameStatus;
+            }
+
+            ttt.MoveNumber = ttt.MoveNumber + 1;
+            (sender as Button).IsEnabled = false;       
+           
+            return;
+           
+        }
+
+        private void SetSquareValue(string thisTag, bool PlayerXTurn)
+        {
+            string row = thisTag.Substring(0, 1);
+            string column = thisTag.Substring(2, 1);
+            string varName = "Square" + row.ToString() + column.ToString();
+            if (varName == "Square00")
+            {
+                if (PlayerXTurn)
+                    ttt.Square00 = "X";
+                else
+                    ttt.Square00 = "O";
+                return;
+            }
+            if (varName == "Square01")
+            {
+                if (PlayerXTurn)
+                    ttt.Square01 = "X";
+                else
+                    ttt.Square01 = "O";
+                return;
+            }
+            if (varName == "Square02")
+            {
+                if (PlayerXTurn)
+                    ttt.Square02 = "X";
+                else
+                    ttt.Square02 = "O";
+                return;
+            }
+            if (varName == "Square10")
+            {
+                if (PlayerXTurn)
+                    ttt.Square10 = "X";
+                else
+                    ttt.Square10 = "O";
+                return;
+            }
+            if (varName == "Square11")
+            {
+                if (PlayerXTurn)
+                    ttt.Square11 = "X";
+                else
+                    ttt.Square11 = "O";
+                return;
+            }
+            if (varName == "Square12")
+            {
+                if (PlayerXTurn)
+                    ttt.Square12 = "X";
+                else
+                    ttt.Square12 = "O";
+                return;
+            }
+            if (varName == "Square20")
+            {
+                if (PlayerXTurn)
+                    ttt.Square20 = "X";
+                else
+                    ttt.Square20 = "O";
+                return;
+            }
+            if (varName == "Square21")
+            {
+                if (PlayerXTurn)
+                    ttt.Square21 = "X";
+                else
+                    ttt.Square21 = "O";
+                return;
+            }
+            if (varName == "Square22")
+            {
+                if (PlayerXTurn)
+                    ttt.Square22 = "X";
+                else
+                    ttt.Square22 = "O";
+                return;
+            }
+        }
+
+
+        private void uxNewGame_Click(object sender, RoutedEventArgs e)
+        {
+
+            // Enable all Squares
+            uxTurn.Text = "Player X Turn";
+            ttt.GameStatus = "Player X Turn";
+            ttt.PlayerXTurn = true;
+            ttt.PlayerOTurn = false;
+            ttt.MoveNumber = 1;
+
+            // Set all Squares in model to null
+            ttt.Square00 = "";         
+            ttt.Square01 = "";
+            ttt.Square02 = "";
+            ttt.Square10 = "";
+            ttt.Square11 = "";
+            ttt.Square12 = "";
+            ttt.Square20 = "";
+            ttt.Square21 = "";
+            ttt.Square22 = "";
+
+            //Clear board
+            //string thisTag = ((Button)sender).Tag.ToString();
+            InitializeComponent();
+            return;
+        }
+    }
+}
---
+++ Homework5/Models/TicTacToeGame.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework5.Models
+{
+    public class TicTacToeGame
+    {
+        public string GameStatus { get; set; }
+        // Game Status values
+        // Player X Turn
+        // Player O Turn
+        // Player X Wins
+        // Player O Won
+        // CAT Game
+
+        public bool PlayerXTurn { get; set; }
+        public bool PlayerOTurn { get; set; }
+
+        //Value is either X or O
+        public string Square00 { get; set; }
+        public string Square01 { get; set; }
+        public string Square02 { get; set; }
+        public string Square10 { get; set; }
+        public string Square11 { get; set; }
+        public string Square12 { get; set; }
+        public string Square20 { get; set; }
+        public string Square21 { get; set; }
+        public string Square22 { get; set; }
+
+        // increment each time tile clicked - max value is 9
+        public int MoveNumber { get; set; }
+
+        // Method that checks if one of 8 winning combinations on each button click
+        // if no match and GameOver - then CAT game
+        // returns gameStatus
+        public string IsAWinner(string gameStatus, int moveNumber)
+        {
+            bool isAWinner = false;
+            if(!String.IsNullOrEmpty(Square00) || !String.IsNullOrEmpty(Square01) || !String.IsNullOrEmpty(Square02))
+                 if ((Square00 == Square01) && (Square01 == Square02))
+                        isAWinner = true;
+            if (!String.IsNullOrEmpty(Square10) || !String.IsNullOrEmpty(Square11) || !String.IsNullOrEmpty(Square12))
+                if ((Square10 == Square11) && (Square11 == Square12))
+                    isAWinner = true;
+            if (!String.IsNullOrEmpty(Square20) || !String.IsNullOrEmpty(Square21) || !String.IsNullOrEmpty(Square22))
+                if ((Square20 == Square21) && (Square21 == Square22))
+                    isAWinner = true;
+            if (!String.IsNullOrEmpty(Square00) || !String.IsNullOrEmpty(Square10) || !String.IsNullOrEmpty(Square20))
+                if ((Square00 == Square10) && (Square10 == Square20))
+                    isAWinner = true;
+            if (!String.IsNullOrEmpty(Square01) || !String.IsNullOrEmpty(Square11) || !String.IsNullOrEmpty(Square21))
+                if ((Square01 == Square11) && (Square11 == Square21))
+                    isAWinner = true;
+            if (!String.IsNullOrEmpty(Square02) || !String.IsNullOrEmpty(Square12) || !String.IsNullOrEmpty(Square22))
+                if ((Square02 == Square12) && (Square12 == Square22))
+                    isAWinner = true;
+            if (!String.IsNullOrEmpty(Square00) || !String.IsNullOrEmpty(Square11) || !String.IsNullOrEmpty(Square22))
+                if ((Square00 == Square11) && (Square11 == Square22))
+                    isAWinner = true;
+            if (!String.IsNullOrEmpty(Square20) || !String.IsNullOrEmpty(Square11) || !String.IsNullOrEmpty(Square02))
+                if ((Square20 == Square11) && (Square11 == Square02))
+                    isAWinner = true;
+
+            // We have a winner
+            if (isAWinner && gameStatus == "Player X Turn")
+                return "Player X Wins";
+            else if (isAWinner && gameStatus == "Player O Turn")
+                return "Player O Wins";
+
+            // No winner yet
+            if (!isAWinner && gameStatus == "Player X Turn" && !GameOver(moveNumber))
+                return "Player O Turn";
+            else if (!isAWinner && gameStatus == "Player O Turn" && !GameOver(moveNumber))
+                return "Player X Turn";
+
+            return "CAT Game";
+        }
+
+        //check if game is over - MoveNumber = 9
+        public bool GameOver(int moveNumber)
+        {
+            return (moveNumber >= 9);
+        }
+
+    }
+}
